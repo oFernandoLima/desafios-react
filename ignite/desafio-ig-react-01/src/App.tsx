711c9d2667c334ed8b3df8dc2456f419a3fc2a51
Fernando Lima <eufernandoprogramador@gmail.com>
2024-03-13 00:43:51 -0300

    feat: Tasks component

--- ignite/desafio-ig-react-01/src/App.tsx
+++ ignite/desafio-ig-react-01/src/App.tsx
@@ -2,6 +2,7 @@ import './App.module.css'
 
 import { Header } from './components/Header'
 import { NewTask } from './components/NewTask'
+import { Tasks } from './components/Tasks'
 
 export function App() {
   return (
@@ -10,6 +11,7 @@ export function App() {
 
       <main>
         <NewTask />
+        <Tasks />
       </main>
     </>
   )
