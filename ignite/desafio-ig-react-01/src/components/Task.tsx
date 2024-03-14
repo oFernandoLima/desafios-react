b047c962fb4938723aefcf30c57595f457e9b50f
Fernando Lima <eufernandoprogramador@gmail.com>
2024-03-14 12:21:58 -0300

    feat: Task component

--- ignite/desafio-ig-react-01/src/components/Task.tsx
+++ ignite/desafio-ig-react-01/src/components/Task.tsx
@@ -1,3 +1,18 @@
+import { Trash, Trash2 } from 'lucide-react'
+
+import styles from './Task.module.css'
+
 export function Task() {
-  return <h1>Task</h1>
+  return (
+    <div className={styles.task}>
+      <button className={styles.doneButton}></button>
+      <p className={styles.taskName}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A provident
+        natus deserunt quidem nemo, alias placeat molestiae qui neque error.
+      </p>
+      <button className={styles.deleteButton}>
+        <Trash2 size={14} />
+      </button>
+    </div>
+  )
 }
